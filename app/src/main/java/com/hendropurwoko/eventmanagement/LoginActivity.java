060d858c51d2f40ad4d43a3ad0f2e62490a623cc
hendroprwk08 <40080703+hendroprwk08@users.noreply.github.com>
2019-12-25 11:54:10 +0700

    ok

--- app/src/main/java/com/hendropurwoko/eventmanagement/LoginActivity.java
+++ app/src/main/java/com/hendropurwoko/eventmanagement/LoginActivity.java
@@ -41,8 +41,8 @@ import java.net.URLEncoder;
  */
 public class LoginActivity extends AppCompatActivity {
 
-    LinearLayout llLogin, llRegistration;
-    EditText etUsername, etPassword, etEmail, etRegisEmail, etRegisPassword, etRegisUsername;
+    LinearLayout llLogin, llRegistration, llVerification;
+    EditText etPassword, etLoginEmail, etRegisEmail, etRegisPassword, etRegisUsername, etVerification;
     ProgressBar pb;
 
     @Override
@@ -54,24 +54,26 @@ public class LoginActivity extends AppCompatActivity {
 
         pb = (ProgressBar) findViewById(R.id.pb);
 
-        etUsername = (EditText) findViewById(R.id.login_et_username);
+        etLoginEmail = (EditText) findViewById(R.id.login_et_email);
         etPassword = (EditText) findViewById(R.id.login_et_password);
-        etEmail = (EditText) findViewById(R.id.regis_et_email);
+        etRegisEmail = (EditText) findViewById(R.id.regis_et_email);
         etRegisPassword = (EditText) findViewById(R.id.regis_et_password);
+        etVerification = (EditText) findViewById(R.id.veri_et_vericode);
 
         llLogin = (LinearLayout) findViewById(R.id.ll_login);
         llRegistration = (LinearLayout) findViewById(R.id.ll_registration);
+        llVerification = (LinearLayout) findViewById(R.id.ll_verification);
 
         final Button btLogin = (Button) findViewById(R.id.login_bt_login);
         btLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if (etUsername.getText().length() == 0 || etUsername.getText().length() == 0){
+                if (etLoginEmail.getText().length() == 0 || etLoginEmail.getText().length() == 0){
                     Toast.makeText(getBaseContext(), "Lengkapi data", Toast.LENGTH_SHORT).show();
                 } else
                 {
-                    login(etUsername.getText().toString().trim(),
+                    login(etLoginEmail.getText().toString().trim(),
                             etPassword.getText().toString().trim());
                 }
             }
@@ -95,6 +97,24 @@ public class LoginActivity extends AppCompatActivity {
                 llRegistration.setVisibility(LinearLayout.GONE);
             }
         });
+
+        final Button btBackVeri = (Button) findViewById(R.id.veri_bt_back);
+        btBackVeri.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                llLogin.setVisibility(LinearLayout.VISIBLE);
+                llVerification.setVisibility(LinearLayout.GONE);
+            }
+        });
+
+        final Button btConfirmVeri = (Button) findViewById(R.id.veri_bt_confirm);
+        btConfirmVeri.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                veri(etVerification.getText().toString().trim(),
+                        etLoginEmail.getText().toString().trim());
+            }
+        });
     }
 
     private void hide() {
@@ -105,14 +125,15 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
-    void login(final String us, String pw){
+    void login(final String em, String pw){
         pb.setVisibility(ProgressBar.VISIBLE);
 
         try {
             String url = Cons.BASE_URL + "login.php?action=1" +
-                    "&us=" + URLEncoder.encode(us, "utf-8") +
+                    "&em=" + URLEncoder.encode(em, "utf-8") +
                     "&pw=" + URLEncoder.encode(pw, "utf-8");
 
+            Log.d(Cons.TAG, "login: " + url);
             RequestQueue queue = Volley.newRequestQueue(getBaseContext());
             JsonObjectRequest jsObjRequest = new JsonObjectRequest(
                     Request.Method.GET,
@@ -123,11 +144,13 @@ public class LoginActivity extends AppCompatActivity {
                         @Override
                         public void onResponse(JSONObject response) {
                             Log.d("Save: ", response.toString());
+                            String status = response.optString("status");
+                            if(status.equals("1")) {
 
-                            String email = null, type = null, username = null, status = response.optString("status").trim();
+                                llLogin.setVisibility(LinearLayout.GONE);
+                                llVerification.setVisibility(LinearLayout.VISIBLE);
 
-                            if(response.optString("status") == "1") {
-                                JSONArray jsonArray = null;
+                                /*JSONArray jsonArray = null;
                                 try {
                                     jsonArray = response.getJSONArray("result");
 
@@ -142,12 +165,100 @@ public class LoginActivity extends AppCompatActivity {
                                     } else if (status.equals("1")) {
                                         saveSharedPreferences(email, type, username);
 
-                                        //buka main app
+
+                                        /*buka main app
                                         Intent i = new Intent(LoginActivity.this, MainAppActivity.class);
                                         i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY); //close login activity
                                         i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                                         getApplicationContext().startActivity(i);
                                     }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }*/
+                            }else{
+                                Toast.makeText(getBaseContext(), "Login gagal!\nData tak ditemukan", Toast.LENGTH_SHORT).show();
+                            }
+
+                            pb.setVisibility(ProgressBar.GONE);
+                        }
+                    }, new Response.ErrorListener() {
+
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    // TODO Auto-generated method stub
+                    Log.d("Events: ", error.toString());
+
+                    pb.setVisibility(ProgressBar.GONE);
+
+                    if (error instanceof TimeoutError || error instanceof NoConnectionError) { //time out or there is no connection
+                        Toast.makeText(getBaseContext(), R.string.time_out_try_again, Toast.LENGTH_SHORT).show();
+                    } else if (error instanceof AuthFailureError) { //there was an Authentication Failure
+                        Toast.makeText(getBaseContext(), R.string.auth_failed, Toast.LENGTH_SHORT).show();
+                    } else if (error instanceof ServerError) { //server responded with a error response
+                        Toast.makeText(getBaseContext(), R.string.server_problem, Toast.LENGTH_SHORT).show();
+                    } else if (error instanceof NetworkError) {//network error while performing the request
+                        Toast.makeText(getBaseContext(), R.string.please_check_your_connection, Toast.LENGTH_SHORT).show();
+                    } else if (error instanceof ParseError) {//the server response could not be parsed
+                        Toast.makeText(getBaseContext(), R.string.reading_data_failed, Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getBaseContext(), R.string.server_problem, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+            queue.add(jsObjRequest);
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+
+            String stackTrace = Log.getStackTraceString(e);
+
+            Toast.makeText(getBaseContext(),
+                    stackTrace,
+                    Toast.LENGTH_SHORT).show();
+
+            pb.setVisibility(ProgressBar.GONE);
+        }
+    }
+
+    void veri(final String veri, String em){
+        pb.setVisibility(ProgressBar.VISIBLE);
+
+        try {
+            String url = Cons.BASE_URL + "login.php?action=11" +
+                    "&veri=" + URLEncoder.encode(veri, "utf-8") +
+                    "&em=" + URLEncoder.encode(em, "utf-8");
+
+            RequestQueue queue = Volley.newRequestQueue(getBaseContext());
+            JsonObjectRequest jsObjRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    url,
+                    null,
+                    new Response.Listener<JSONObject>() {
+
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            Log.d("Save: ", response.toString());
+                            String email = null, type = null, username = null, status = response.optString("status").trim();
+
+                            if(status.equals("1")) {
+                                JSONArray jsonArray = null;
+                                try {
+                                    jsonArray = response.getJSONArray("result");
+
+                                    JSONObject data = jsonArray.getJSONObject(0);
+                                    email = data.getString("email").toString().trim();
+                                    type = data.getString("type").toString().trim();
+                                    username = data.getString("username").trim();
+
+                                    saveSharedPreferences(email, type, username);
+
+                                    //buka main app
+                                    Intent i = new Intent(LoginActivity.this, MainAppActivity.class);
+                                    i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY); //close login activity
+                                    i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                    getApplicationContext().startActivity(i);
+
                                 } catch (JSONException e) {
                                     e.printStackTrace();
                                 }
